4c079d0a4cff7fdd9483fa37f622a3709d01a527
Oghenevwede Louis Umukoro <wedevilleg@gmail.com>
2024-11-18 13:17:50 +0100

    responsive

--- src/components/Hero/Heroone.jsx
+++ src/components/Hero/Heroone.jsx
@@ -129,45 +129,44 @@ const Heroone=()=>{
             </div>
 
             {/* Power wall */}
-            <div className="bg-cover bg-center bg-no-repeat h-screen rounded-lg mx-[1rem] md:mx-[2rem] lg:mx-[3rem] mt-[2rem] " style={{ backgroundImage: "url('/images/wp.jpg')" }}>
-                <div className=" items-center flex flex-col w-full pt-8">
+            <div className="bg-cover bg-center bg-no-repeat h-screen" style={{ backgroundImage: "url('/images/wp.jpg')" }}>
+                <div className=" items-center flex flex-col w-full pt-16">
                     <div className="flex flex-col items-center gap-3">
-                        <p className="text-[2rem] lg:text-[3rem] text-neutral-500 font-bold ">Powerwall</p>
+                        <p className="text-[2rem] lg:text-[3rem] text-white font-bold ">Powerwall</p>
                     </div>
 
 
-                    <div className="md:pt-[20rem] lg:pt-[21rem] xl:pt-[26rem] pt-72 md:block  gap-2  m-auto flex flex-col">
-                        <button className="bg-blue-600 text-white font-semibold mt-2 text-[10px] py-2 px-16 rounded-[8px] ml-6">Order Now</button>
-                        <button className="bg-white text-neutral-600 mt-2 font-semibold text-[10px] py-2 px-16 rounded-[8px] ml-6">Learn more</button>
+                    <div className="pt-[3rem] md:pt-[8rem] lg:pt-[17rem] xl:pt-[11rem] justify-center gap-4 flex w-full items-center">
+                        <button className="bg-blue-600 text-white font-semibold  text-[10px] py-2 px-8 rounded-[8px] ">Order Now</button>
+                        <button className="bg-white text-neutral-600 font-semibold text-[10px] py-2 px-8 rounded-[8px] ">Demo Drive</button>
                     </div>
                 </div>
             </div>
 
             {/* Accessories */}
-            <div className="bg-cover bg-center bg-no-repeat h-screen rounded-lg mx-[1rem] md:mx-[2rem] lg:mx-[3rem] mt-[2rem] " style={{ backgroundImage: "url('/images/charger.jpg')" }}>
-                <div className=" items-center flex flex-col w-full pt-8">
+            <div className="bg-cover bg-center bg-no-repeat h-screen" style={{ backgroundImage: "url('/images/charger.jpg')" }}>
+                <div className=" items-center flex flex-col w-full pt-16">
                     <div className="flex flex-col items-center gap-3">
-                        <p className="text-[2rem] lg:text-[3rem] text-neutral-500 font-bold ">Accessories</p>
+                        <p className="text-[2rem] lg:text-[3rem] text-white font-bold ">Accessories</p>
                     </div>
 
 
-                    <div className="md:pt-[20rem] lg:pt-[21rem] xl:pt-[26rem] pt-72 md:block  gap-2  m-auto flex flex-col">
-                        <button className="bg-blue-600 text-white font-semibold mt-2 text-[10px] py-2 px-16 rounded-[8px] ml-6">Shop Now</button>
-                        
+                    <div className="pt-[3rem] md:pt-[8rem] lg:pt-[17rem] xl:pt-[11rem] justify-center gap-4 flex w-full items-center">
+                        <button className="bg-blue-600 text-white font-semibold  text-[10px] py-2 px-8 rounded-[8px] ">Shop Now</button>
                     </div>
                 </div>
             </div>
 
             {/* we are tesla */}
-            <div className=" mx-[1rem] md:mx-[2rem] lg:mx-[3rem] mt-[1rem]">
-                <div className=" items-center flex flex-col w-full pt-8 relative">
+            <div className="">
+                <div className=" items-center flex flex-col w-full relative">
                     
                     <div className="">
-                        <video src="/videos/teslav.mp4" width={0} height={0} alt='video' className="w-[100rem] rounded-lg" controls/>
+                        <video src="/videos/teslav.mp4" width={0} height={0} alt='video' className="w-[100vw]" controls/>
                     </div>
 
-                    <div className="md:hidden absolute left-[1rem] bottom-[5rem] text-neutral-500 w-fit ">
-                        <h1 className="text-sm font-bold">We are Tesla</h1>
+                    <div className="absolute left-[1rem] bottom-[5rem] text-neutral-500 w-fit ">
+                        <h1 className="text-sm font-bold text-white">We are Tesla</h1>
                         <button className="bg-slate-200 text-zinc-800 text-sm w-full py-1 mt-1 rounded-md ">Join Tesla</button>
                     </div>
 
